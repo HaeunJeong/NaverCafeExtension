bab18613831613e3e1c6e1a2118ff769631e1050
HaeunJeong <nicehe74@gmail.com>
2022-10-04 00:18:27 +0900

    깔끔하게 정리

--- background.js
+++ background.js
@@ -6,29 +6,3 @@ chrome.runtime.onInstalled.addListener(function(message, callback) {
         });
     }
 });
-
-async function getSearchUrl() {
-
-    let allArticleUrl = "https://cafe.naver.com/ArticleList.nhn?search.clubid=28144209&amp;search.boardtype=L";
-    let boardNum = 20; // 가입인사 
-    let pageNum = 1;
-
-    let resultUrl = allArticleUrl +
-        "&search.page=" + pageNum +
-        boardNum != null ? "&search.menuid=" + boardNum : "";
-
-    console.log("resultUtl = " + resultUrl);
-
-    /*
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: () => {
-
-            //console.log(new window.DOMParser().parseFromString(document, "text/xml"));
-            console.log(document.querySelector('iframe'));
-        }
-    });
-    */
-
-    return resultUrl;
-}
